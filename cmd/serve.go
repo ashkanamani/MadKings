65f4eb7565eed404056bbdf42e9546cab4a9fb94
AshkanAmani <amaniashkan2000@gmail.com>
2025-10-17 10:10:10 +0330

    first commit

---
+++ cmd/serve.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "A brief description of your command",
+	Run:   serve,
+}
+
+func serve(cmd *cobra.Command, args []string) {
+	fmt.Println("serve called")
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+}
